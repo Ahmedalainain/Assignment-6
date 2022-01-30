426c5951fb7964ff6c6babe62baf18ea5dc8721c
Ahmmmmmed99 <ahmedalainain@gmail.com>
2022-01-29 19:07:34 -0500

    asa
    SDS

--- zip-search/src/zip-code.js
+++ zip-search/src/zip-code.js
@@ -2,7 +2,27 @@ import React, {useState} from "react";
 function ZipCode (props)
  {
     const {zip, setZip, list, setList} = props
-    return(<><h2>{zip}<button onclick="myFunction()">Click me</button></h2></>)
-    
-}
-export default ZipCode;
+    return(<><h2>{zip}<button onclick="myFunction()">Click</button></h2>
+       <div>
+ <ul className=" container">
+        <li className="info">city:{DataTransfer.City}</li>
+        <li className="info">location: {DataTransfer.City}</li>
+        <li className="info">state: {DataTransfer.City}</li>
+       </ul>
+        </div>
+        </>)}
+        
+        
+        rrrrrrrrrr{list.map((element) 
+            return (
+                <ul className="
+            < li  classname=">ctiy{element.City
+            <li classname=">location{element.location
+                <li classname=">state{element.state
+
+
+
+                li  classname=">location</ul>
+
+
+            )
